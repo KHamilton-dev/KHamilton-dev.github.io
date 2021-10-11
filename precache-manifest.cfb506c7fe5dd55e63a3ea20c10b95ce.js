da37172f6742e3bdfabb11fb38fe91d562b67e2b
Kevin Hamilton <77547818+KHamilton-dev@users.noreply.github.com>
2021-10-11 13:26:00 -0600

    Updates

---
+++ precache-manifest.cfb506c7fe5dd55e63a3ea20c10b95ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dd69d84e1109785d72b",
+    "url": "/Portfolio/static/js/main.3dd69d84.chunk.js"
+  },
+  {
+    "revision": "d5a82794cb252de57f41",
+    "url": "/Portfolio/static/js/1.d5a82794.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "e8c1aa1d200890eac748954d9a26d8cc",
+    "url": "/Portfolio/index.html"
+  }
+];
